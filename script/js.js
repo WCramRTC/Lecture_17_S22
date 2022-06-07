b741ba31b2358c90a3a7096f550c1656de5a2626
WCram <wcram@rtc.edu>
2022-06-07 09:53:27 -0700

    Initial commit

---
+++ script/js.js
@@ -0,0 +1,11 @@
+const arr = [];
+arr[0] = 1;
+arr[1] = "string";
+arr[5] = true;
+// arr["donkey"] = "cat";
+
+let person = {firstName: "Will", lastName: "Cram"};
+
+alert(`Hello ${person.firstName} ${person.lastName}`);
+
+// arr.forEach(element => console.log(element));
